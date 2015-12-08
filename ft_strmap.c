35ace82294df69c769c1c9f190dd4a715ae9ebf5
Paul BIE <pbie@e1r7p13.42.fr>
2015-12-08 17:15:55 +0100

    libft protected

---
+++ ft_strmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbie <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/27 18:40:57 by pbie              #+#    #+#             */
+/*   Updated: 2015/12/08 16:30:30 by pbie             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbie <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/27 19:00:25 by pbie              #+#    #+#             */
+/*   Updated: 2015/12/08 16:17:40 by pbie             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strtrim(char const *s)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+	char			*str;
+
+	i = 0;
+	k = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	if (s[i] == '\0')
+		return (ft_strcpy(ft_memalloc(sizeof(char) * 2), ""));
+	j = ft_strlen(s) - 1;
+	while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
+		j--;
+	if (!(str = (char *)malloc(sizeof(char) * (j - i + 2))))
+		return (NULL);
+	while (k < j - i + 1)
+	{
+		str[k] = s[i + k];
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
